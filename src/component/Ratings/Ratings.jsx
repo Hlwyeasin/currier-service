72b299dbaa2c9db4a154d675568cfe5722e2754b
yeasinChowdhury <depressedman996@gmail.com>
2025-07-18 08:26:42 +0600

    add new change

--- src/component/Ratings/Ratings.jsx
+++ src/component/Ratings/Ratings.jsx
@@ -1,72 +1,60 @@
-// import React from "react";
-// import Containner from "../Containner/Containner";
-// import ratings1 from "../../assets/ratings1.svg";
-// import ratings2 from "../../assets/ratings2.svg";
-// import ratings3 from "../../assets/ratings3.svg";
-// import ratings4 from "../../assets/ratings4.svg";
-// import ratings5 from "../../assets/ratings5.svg";
-// import imge from "../../assets/footerLogo.png";
-
-// const Ratings = () => {
-//   const ratingData = [
-//     {
-//       img: ratings1,
-//       title: "26+",
-//       text: "Awards  won",
-//     },
-//     {
-//       img: ratings2,
-//       title: "65+",
-//       text: "States covered",
-//     },
-//     {
-//       img: ratings3,
-//       title: "689K+",
-//       text: "Happy clients",
-//     },
-//     {
-//       img: ratings4,
-//       title: "130M+",
-//       text: "Goods delivered",
-//     },
-//     {
-//       img: ratings5,
-//       title: "130M+",
-//       text: "Business hours",
-//     },
-//   ];
-//   return (
-//     <div className="bg-section pb-[162px]">
-//       <Containner>
-//         <div className="flex justify-between items-center">
-//           {ratingData.map((index) => (
-//             <div className="text-center">
-//               <img src={imge} alt="" className="mx-auto" />
-//               <h1 className="font-extrabold text-[39px] leading-[44px] text-primary pt-[28px] pb-[18px]">
-//                 {index.title}
-//               </h1>
-//               <p className="font-primary font-bold text-[20px] leading-[120%] text-[#464558]">
-//                 {index.text}
-//               </p>
-//             </div>
-//           ))}
-//         </div>
-//       </Containner>
-//     </div>
-//   );
-// };
-
-// export default Ratings;
-
-import React from 'react'
+import React from "react";
+import Containner from "../Containner/Containner";
+import raginh01 from "../../assets/ratings1.svg";
+import rating02 from "../../assets/ratings2.svg";
+import rating03 from "../../assets/ratings3.svg";
+import rating04 from "../../assets/ratings4.svg";
+import raging05 from "../../assets/ratings5.svg";
 
 const Ratings = () => {
+  const ratingData = [
+    {
+      img: raginh01,
+      title: "26+",
+      text: "Awards  won",
+    },
+    {
+      img: rating02,
+      title: "65+",
+      text: "States covered",
+    },
+    {
+      img: rating03,
+      title: "689K+",
+      text: "Happy clients",
+    },
+    {
+      img: rating04,
+      title: "130M+",
+      text: "Goods delivered",
+    },
+    {
+      img: raging05,
+      title: "130M+",
+      text: "Business hours",
+    },
+  ];
   return (
-    <div>
-      Ratings
-    </div>
-  )
-}
+    <>
+      <div className="bg-section pb-[162px]">
+        <Containner>
+          <div className="flex justify-between items-center">
+            {ratingData.map((index) => (
+              <div className="text-center">
+                <img src={index.img} alt="" className="mx-auto" />
+                <h1 className="font-extrabold text-[39px] leading-[44px] text-orange-500 pt-[28px] pb-[18px]">
+                  {index.title}
+                </h1>
+                <p className="font-primary font-bold text-[20px] leading-[120%] text-[#464558]">
+                  {index.text}
+                </p>
+              </div>
+            ))}
+          </div>
+        </Containner>
+      </div>
+    </>
+  );
+};
 
-export default Ratings
-
+export default Ratings;
--- src/component/Services/Services.jsx
+++ src/component/Services/Services.jsx
@@ -1,11 +1,100 @@
 import React from 'react'
+import serviceimg from "../../assets/service1.svg";
+import sevice02img from "../../assets/service2.svg";
+import service03img from "../../assets/service3.svg";
+import Containner from '../Containner/Containner';
+
+
 
 const Services = () => {
+
+   const data = [
+        {
+            img: serviceimg,
+            title:"Business Services" ,
+            text: "We give you complete reliable delivery for your company.  We will take full responsibility of the deliveries.",
+            ul:[
+                 "Corporate goods",
+                 "Shipment",
+                 "Accesories"
+            ],
+            button: "Learn more"
+        },
+        {
+            img: sevice02img,
+            title:"Statewide Services" ,
+            text: "Offering home delivery around the city, where your products will be at your doorstep within 48-72 hours.",
+            ul:[
+                 "Personal items",
+                 "Percels",
+                 "Documents"
+            ],
+            button: "Learn more"
+        },
+        {
+            img: service03img,
+            title:"Personal Services" ,
+            text: "You can trust us to safely deliver your most sensitive documents to the specific area in a short time.",
+            ul:[
+                 "Gifts",
+                 "Package",
+                 "Documents"
+            ],
+            button: "Learn more"
+        },
+    ]
   return (
-    <div>
-      Services
+    <>
+
+    <div className='bg-section pb-[172px]'>
+        <Containner>
+
+            <div className='text-center'>
+                <h1 className='font-secondery font-bold text-[31px] leading-[100%] text-primary'>SERVICES</h1>
+                <h2 className='font-primary font-extrabold text-[49px] leading-[100%] pt-[11px] pb-[86px]'>Our services for you</h2>
+            </div>
+
+
+            <div className='flex justify-between'>
+                {
+                    data.map((item,i)=>(
+
+                        <div key={i} className=' group w-[424px] bg-white rounded-[16px] pb-[49px] pt-[67px] px-[60px] hover:shadow-lg'>
+                          
+                            <img src={item.img} alt="" className='mx-auto' />
+                            <h2 className='font-primary font-extrabold text-[25px] leading-[100%] pt-[29px] pb-8 text-center'>{item.title}</h2>
+                            <p className='font-primary text-base leading-[145%] text-[#7B7A8B] pb-[25px]'>{item.text}</p>
+                            {
+                                item.ul.map((index,idx)=>(
+                                    <div key={idx} className='flex items-center gap-x-[10px] '>
+                                        <div className='w-[7px] h-[7px] rounded-full bg-primary'></div>
+                                        <div className='font-primary text-base leading-[120%] text-[#7B7A8B] py-2'>{index}</div>
+                                    </div>
+                                ))
+                            }
+                            <div className='font-secondery font-bold text-[20px] leading-[100%] text-black border border-primary py-[12px] px-[96px] rounded-[5px] mt-[60px] group-hover:bg-primary group-hover:text-blue-600'>
+                                <a href="" >{item.button}</a>
+                            </div>
+                        </div>
+
+                    ))
+                }
+            </div>
+
+
+
+        </Containner>
     </div>
+      
+    </>
   )
 }
 
-export default Services
+export default Services;
+
+
+
+
+
+
+
